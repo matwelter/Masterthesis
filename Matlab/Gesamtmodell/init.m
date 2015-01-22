61bbc8f28a73e45170cd3b125e444516f59b44eb
matwelter <mat.welter@gmail.com>
2015-01-22 21:24:56 +0100

    Begin : Kap Simulation sim_grafiken

--- Matlab/Gesamtmodell/init.m
+++ Matlab/Gesamtmodell/init.m
@@ -9,9 +9,13 @@ bias = [0;0;0];
 % l_1 = 8; 
 % l_2 = 15.18;
 % l_3 = 25.45;
-l_1 = 8; 
-l_2 = 10.18;
-l_3 = 25.45;
+l_1 = 18; 
+l_2 = 46.18;
+l_3 = 3.75;
+% l_1 = 18.1; 
+% l_2 = 45.18;
+% l_3 = 0.45;
+
 
 %% Referenzmodell
 omega_0 = 1.5;
@@ -27,11 +31,14 @@ Ki = 0.01;
 % Kd = 6;
 % Kp = 1.2;
 % Ki = 0.06
-sim('gesamtmodell')
+% Kd = 1;
+% Kp = 2;
+% Ki = 0.01;
+%sim('gesamtmodell')
 
-figure('Name','ref_vor_id');
-    plot(P.time,P.signals.values(:,1),P_ref.time,P_ref.signals.values(:,1),'g',P_mdl.time,P_mdl.signals.values(:,1),'r');
-            xlabel('sec');
-            ylabel('m');
-            legend('p_{cmd}','p_{ref}','p_{mdl}');
+%figure('Name','ref_vor_id');
+  %  plot(P.time,P.signals.values(:,1),P_ref.time,P_ref.signals.values(:,1),'g',P_mdl.time,P_mdl.signals.values(:,1),'r');
+  %          xlabel('sec');
+  %          ylabel('m');
+  %          legend('p_{cmd}','p_{ref}','p_{mdl}');
 
--- Matlab/Gesamtmodell/plots_gesmdl.m
+++ Matlab/Gesamtmodell/plots_gesmdl.m
@@ -34,3 +34,52 @@ figure('Name','ref_vor_id');
             xlabel('sec');
             ylabel('m');
             legend('p_{cmd}','p_{ref}','p_{mdl}');
+            
+%% Für Kapitel Simulation Gesamtmodell            
+  %Pos x          
+        figure('Name','Positionen ');
+    plot(P_40.time,P_40.signals.values(:,1),'b',P_bo.time,P_bo.signals.values(:,1),'r',P.time,P.signals.values(:,1),'m',P_ref.time,P_ref.signals.values(:,1),'g');
+            xlabel('sec');
+            ylabel('m');
+            legend('p_{40Hz}','p_{beob}','p_{cmd}','p_{ref}');    
+            
+    %Pos y        
+            figure('Name','Positionen ');
+    plot(P_40.time,P_40.signals.values(:,2),'b',P_bo.time,P_bo.signals.values(:,2),'r',P.time,P.signals.values(:,2),'m',P_ref.time,P_ref.signals.values(:,2),'g');
+            xlabel('sec');
+            ylabel('m');
+            legend('p_{40Hz}','p_{beob}','p_{cmd}','p_{ref}');   
+            
+    %V x
+    
+      figure('Name','Geschwindigkeit');
+    plot(V_bo.time,V_bo.signals.values(:,1),'r',V_ref.time,V_ref.signals.values(:,1),'g');
+            xlabel('v_x in m/sec');
+            ylabel('m');
+            legend('v_{beob}','v_{ref}');
+            
+     %V x
+    
+      figure('Name','Geschwindigkeit');
+    plot(V_bo.time,V_bo.signals.values(:,2),'r',V_ref.time,V_ref.signals.values(:,2),'g');
+            xlabel('sec');
+            ylabel('v_y in m/s');
+            legend('v_{beob}','v_{ref}');       
+            
+     %Ori       
+            
+   figure('Name','Winkel');
+    plot(out_invers.time,out_invers.signals.values(:,2),'k');
+            xlabel('sec');
+            ylabel('rad');
+            legend('phi');   
+   
+   figure('Name','Winkel');
+    plot(out_invers.time,out_invers.signals.values(:,3),'k');
+            xlabel('sec');
+            ylabel('rad');
+            legend('theta');            
+            
+            
+            
+            
